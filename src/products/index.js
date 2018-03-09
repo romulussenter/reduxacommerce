abf059fedbd3f914f455adb04f7e04f3ada69db1
Romulus <romulus.senter@yahoo.com>
2018-03-08 19:32:36 -0500

    connected the to my components

--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,23 @@
 
-
-const reducer = (state={}, action) => {
+const initialState = [
+    {
+        imgSrc: 'https://viaplaceholder.com/100x100',
+        name: 'Javelin (warning)',
+        price: 500.00,
+        count: 0
+     },
+    { imgSrc: 'https://viaplaceholder.com/100x100',
+      name: 'snail',
+      price: 3.99,
+      count: 0
+},
+    {imgSrc: 'https://viaplaceholder.com/100x100',
+    name: 'Rug',
+    price: 99.00,
+    count: 0
+}
+];
+const reducer = (state=initialState, action) => {
     console.log(' Product stuff');
     return state;
 } 
