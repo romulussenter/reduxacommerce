ee385f63c528aeab14dbd4517165565e7331fd4d
Romulus <romulus.senter@yahoo.com>
2018-03-12 18:38:45 -0400

    increment and decrement works wit hthe object

--- src/products/index.js
+++ src/products/index.js
@@ -1,22 +1,21 @@
-import * as types from './types';
-import * as productActions from './actions';
-
+import * as types from "./types";
+import * as productActions from "./actions";
 
 const initialState = {
-  'ff6fdc7a-3fa1-45da-803c-b1eeedf44a05':{
+  "ff6fdc7a-3fa1-45da-803c-b1eeedf44a05": {
     imgSrc: "https://via.placeholder.com/100x100",
     name: "Javelin (warning)",
     price: 500.0,
     count: 0
   },
- 
-  '404fba0b-2592-4999-bd20-d0bc03165214':{
+
+  "404fba0b-2592-4999-bd20-d0bc03165214": {
     imgSrc: "https://via.placeholder.com/100x100",
     name: "snail",
     price: 3.99,
     count: 0
   },
-  '29ebcb1c-5f23-4c51-9161-b773ed52fa1f':{
+  "29ebcb1c-5f23-4c51-9161-b773ed52fa1f": {
     imgSrc: "https://via.placeholder.com/100x100",
     name: "Rug",
     price: 99.0,
@@ -24,46 +23,36 @@ const initialState = {
   }
 };
 const reducer = (state = initialState, action) => {
-    switch(action.type){
-      case types.ADD_ITEM:{
-      const position = action.payload.value;
-      const selectedItem = state[position];
-      const before = state.slice(0, position);
-      const after = state.slice(position + 1);
+  switch (action.type) {
+    case types.ADD_ITEM: {
+      const id = action.payload.value;
+      const selectedItem = state[id];
       const newProduct = {
         ...selectedItem,
-      count: selectedItem.count + 1
-    };
-      return [
-        ...before,
-        newProduct,
-        ...after
-      ];
+        count: selectedItem.count + 1
+      };
+      return {
+        ...state,
+        [id]: newProduct
+      };
     }
-      case types.REMOVE_ITEM:{
-      const position = action.payload.value;
-      const selectedItem = state[position];
-      const before = state.slice(0, position);
-      const after = state.slice(position + 1);
+    case types.REMOVE_ITEM: {
+      const id = action.payload.value;
+      const selectedItem = state[id];
       const newProduct = {
         ...selectedItem,
-      count: selectedItem.count === 0 ? 0 : selectedItem.count - 1
-    };
-    return [
-      ...before,
-      newProduct,
-      ...after
-    ];
+        count: selectedItem.count === 0 ? 0 : selectedItem.count - 1
+      };
+      return {
+        ...state,
+        [id]: newProduct,
+      };
+    }
+    default:
+     return state;
   }
-      default:
-      state;
-  return state;
-    }
 };
 
-export {
-  productActions
-};
-
+export { productActions };
 
 export default reducer;
